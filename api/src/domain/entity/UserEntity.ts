d3dce0f90d9a577efb0221100c9b8a0981694c70
Igor Melo <igorjmelo4@gmail.com>
2024-02-07 20:37:44 -0300

    adding props to user entity

--- api/src/domain/entity/UserEntity.ts
+++ api/src/domain/entity/UserEntity.ts
@@ -1,1 +1,22 @@
-export default class UserEntity{}
+export default class UserEntity{
+    constructor(
+        readonly userId: number,
+        readonly name: string | null,
+        readonly email: string,
+        readonly password: string,
+        readonly pictureUrl: string | null,
+        readonly created_at: Date,
+        readonly updated_at: Date,
+    ) {}
+    static create(email: string, password: string): UserEntity {
+        return new UserEntity(
+            0,
+            null,
+            email,
+            password,
+            null,
+            new Date(),
+            new Date()
+        )
+    }
+}
